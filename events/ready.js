eee88ddfff36b67e1491b372f458032b43e0fbeb
Meriem Soubih <soubih.meriem@gmail.com>
2024-06-20 08:20:04 +0100

    not working: bot activity status

--- events/ready.js
+++ events/ready.js
@@ -5,5 +5,6 @@ module.exports = {
 	once: true,
 	execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
+        client.user.setActivity('/help & /quote', { type: 'LISTENING' });
 	},
 };
